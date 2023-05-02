ec65d2a4e61bdee56bcf82dc47c827c424042c21
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-02 10:13:50 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
+
