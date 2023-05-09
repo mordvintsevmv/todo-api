0b17c002b9f367e443313decd744d6c08a025788
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-09 16:44:22 +0400

    Updating server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
+const fs = require('fs')
 
 const server = jsonServer.create()
 
-const fs = require('fs')
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
 const router = jsonServer.router(db)
 
@@ -13,6 +13,17 @@ const middlewares = jsonServer.defaults()
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
+
+server.use((req, res, next) => {
+    if ((req.method === 'POST') || (req.method === "PUT")) {
+        if (!req.body.description)
+            req.body.description = ""
+        if (!req.body.subtasks)
+            req.body.subtasks = []
+    }
+    next()
+})
+
 server.use(router)
 
 const PORT = 8000
